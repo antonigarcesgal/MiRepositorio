43b7abe52cea3b0786edeb732e153cff7c614924
Admin <Admin@10.250.5.6>
2023-04-24 10:48:16 +0200

    Test change

--- java.training.dia2/src/training/lab2/list/Lista.java
+++ java.training.dia2/src/training/lab2/list/Lista.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 public class Lista {
 
+	//TEST COMMITS
+	
 	public static void main(String[] args) {
 
 		//Crear lista
@@ -15,10 +17,10 @@ public class Lista {
 		lista.add("Leccion 2 - Programacion Orientada a Objetos");
 		lista.add("Leccion 3 - JEE");
 		
-		//tamaño lista
-		System.out.println("Tamaño de la lista: " + lista.size());
+		//tamaï¿½o lista
+		System.out.println("Tamaï¿½o de la lista: " + lista.size());
 		
-		//enseña elementos de lista
+		//enseï¿½a elementos de lista
 		System.out.println("Primer elemento de la lista: " + lista.get(0));
 		System.out.println("Segundo elemento de la lista: " + lista.get(1));
 		
@@ -31,7 +33,7 @@ public class Lista {
 		
 		lista.remove(2);
 		
-		System.out.println("Elementos de la lista después de eliminar el tercero:");
+		System.out.println("Elementos de la lista despuï¿½s de eliminar el tercero:");
 		
 		for(String elemento : lista) {
 			System.out.println(elemento);
